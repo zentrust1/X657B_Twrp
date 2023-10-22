ee500c6415789e53d465be5e31382b333883d9d3
Massatrio16 <146039181+Massatrio16@users.noreply.github.com>
2023-10-22 17:52:23 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -8,9 +8,12 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-recovery
 
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
+    android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service \
     libhealthd.$(PRODUCT_PLATFORM)
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctrl.mt6761
 
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
@@ -23,9 +26,15 @@ PRODUCT_PACKAGES += \
 
 # Fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-fastbootd
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mtk.so
 
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
 
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     $(LOCAL_PATH)/security/infinix
